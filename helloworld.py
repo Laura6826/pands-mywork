0f56e3ddfd66f708892f46042dd6ed1a0bae60ea
Laura Lyons <maxwell6826@yahoo.co.uk>
2024-02-07 21:26:27 +0000

    week 02

--- helloworld.py
+++
@@ -1,5 +0,0 @@
-# Hello World
-# A program that will print/output Hello World
-# Author Laura Lyons
-
-print("Hello World")
--- math.py
+++
@@ -1,7 +0,0 @@
-# Math
-# Messing around with Math
-# Author Laura Lyons
-
-floatnum= 0.29
-answer = floatnum * 100
-print(answer)
