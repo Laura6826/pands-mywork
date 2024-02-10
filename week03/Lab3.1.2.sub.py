cbc4bd974182d3016348daaabe5354d40990c4f0
Laura Lyons <maxwell6826@yahoo.co.uk>
2024-02-10 07:35:58 +0000

    10.2.24 07:53

---
+++ week03/Lab3.1.2.sub.py
@@ -0,0 +1,9 @@
+# sub.py
+# The aim of this program is to read in two numbers and subtract the first from the second.
+# Author: Laura Lyons
+
+x = int(input('Enter first number: '))
+y = int(input('Enter second number: '))
+answer = x-y
+print ("{} minus {} is {}" .format(x, y, answer))
+
