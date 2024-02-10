408dfa3fd5c08db6340f4d43812ef9e1334925db
Laura Lyons <maxwell6826@yahoo.co.uk>
2024-02-10 07:13:10 +0000

    10.2.24 07:12

---
+++ week03/Lab3.1.test_types.py
@@ -0,0 +1,15 @@
+# test_types
+# working with various types on inputs
+# Author: Laura Lyons
+
+i = 3
+fl = 3.5
+isa = True
+memo = 'how now Brown Cow'
+lots = []
+
+print('Variable {} is of type:{} and value:{}'.format('i', type(i), i))
+print('Variable {} is of type:{} and value:{}'.format('fl', type(fl), fl))
+print('Variable {} is of type:{} and value:{}'.format('is', type(isa), isa))
+print('Variable {} is of type:{} and value:{}'.format('memo', type(memo), memo))
+print('Variable {} is of type:{} and value:{}'.format('lots', type(lots), lots))
