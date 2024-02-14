ddd7380bb7cb22d9a4aafc9320e83466af61d11e
Laura Lyons <maxwell6826@yahoo.co.uk>
2024-02-14 10:55:36 +0000

    practice

---
+++ week04/test_boolean.py
@@ -0,0 +1,18 @@
+# test_boolean.py
+# booleans can be Tre#ue or False
+# boolean commands == >< >= <=
+# Author: Laura Lyons
+
+is_alive = True
+print(f'is_alive = {is_alive}')
+
+var = (2!=4)
+print(f'var is {var}')
+
+
+logic = (2<=100) and (3>= 100)
+print (f'logic is {logic}')
+
+grade = 40
+invalid = (grade<0) or (grade >100)
+print (f'invalid is {invalid}')
