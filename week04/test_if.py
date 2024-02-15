db8ddebfe2b22b075a9282d913a986da93d0d7b0
Laura Lyons <maxwell6826@yahoo.co.uk>
2024-02-15 16:16:00 +0000

    15.02.24 16:15

---
+++ week04/test_if.py
@@ -0,0 +1,13 @@
+# test_if.py
+# if condition :
+#   statements (if true)
+#
+#if conditional:
+#   statement (if true)
+# elif condition 2
+#   statements (if condition1 is false but 2 is true)
+#else:
+#   statement(if both are false)
+
+# Author Laura Lyons
+
