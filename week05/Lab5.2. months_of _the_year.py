770ff03ffb72f044636b21e88d898a3a37f3e7b0
Laura Lyons <maxwell6826@yahoo.co.uk>
2024-02-27 14:02:32 +0000

    lab 5.2 complete

---
+++ week05/Lab5.2. months_of _the_year.py
@@ -0,0 +1,11 @@
+# months_of_the_year.py
+# The aim of this program is the out put the months of summer
+# Author: Laura Lyons
+
+months=('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December')
+print (months)
+
+summer_months = months[4:7]
+print (summer_months)
+for month in summer_months:
+    print(month)
