5c12445d23c31953503feadc9e7dbd4ea23c0e0d
Laura Lyons <maxwell6826@yahoo.co.uk>
2024-03-03 00:14:19 +0000

    03.03.24

---
+++ week06/Lab6.2.py
@@ -0,0 +1,11 @@
+# wrtie a function out a menu on commands we can perform ie add and quit.
+
+
+
+def command_menu():
+    command_menu: ['a','v','q']
+    if command_menu(a):
+        print()
+   a,v,q= {'Add new student', 'View students', 'Quit'} 
+    print(command_menu)
+
